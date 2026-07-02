2b32511f07b5cee70abe034cc859d3ff77bd1467
hannahjblair <134324294+hannahjblair@users.noreply.github.com>
2026-07-01 20:01:26 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+//I want the form to appear when the user clicks where they want their message to go on the screen
+//and when the form shows everything else should be hidden
+//then i want their message and the info about them to go to the spot they clicked

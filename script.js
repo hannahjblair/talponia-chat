570cc590cd429ba0fc14634d32f7de870242c61f
hannahjblair <134324294+hannahjblair@users.noreply.github.com>
2026-07-03 19:33:15 -0400

    gets rid of the content in the clickable area container thats not the form when user clicks in the container then shows it agai once submit button is clicked

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //TO DO:
-//clear input from form once submitted
 //form validation
 //store form input in an array
 //display form input on page
@@ -7,6 +6,8 @@
 //changes the display of the form element to flex 
 function showForm() {
     document.getElementById("form").style.display = "flex";
+    //hides the other content (right now just one <p>)
+    document.getElementsByClassName("show")[0].style.display = "none";
 }
 
 //changes the display of the form element to none and stops event propogation
@@ -14,17 +15,14 @@ function hideForm(event) {
     event.stopPropagation();
     document.getElementById("form").style.display = "none";
     document.getElementById("form").reset();
+    //shows the other content (right now just one <p>)
+    document.getElementsByClassName("show")[0].style.display = "flex";
 }
 
 //stops the event propogation when there is a click inside form so it doesnt count as a click in clickable-area
 document.getElementById("form").addEventListener("click", function(event) {
     event.stopPropagation();
 });
-
-//clear the inuput
-// function clearForm() {
-//     document.getElementsByClassName("user-input").value = "";
-// }
 
 
 //calls showForm function when user clicks in clickable-area container

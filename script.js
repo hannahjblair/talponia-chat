e0ac32bce5714ac78473590125d2f3d23240a45d
hannahjblair <134324294+hannahjblair@users.noreply.github.com>
2026-07-18 15:07:41 -0400

    edited form, added global array to hold the form answers, added function setContent to assign the form andswers as variables, add them to an object, and push the object up to the array that holds the form's answers

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-//TO DO:
-//form validation
-//store form input in an array
-//display form input on page
-
 //changes the display of the form element to flex 
 function showForm() {
     document.getElementById("form").style.display = "flex";
@@ -15,7 +10,7 @@ function hideForm(event) {
     event.stopPropagation();
     document.getElementById("form").style.display = "none";
     document.getElementById("form").reset();
-    //shows the other content (right now just one <p>gets )
+    //shows the other content 
     document.getElementsByClassName("show")[0].style.display = "flex";
 }
 
@@ -28,6 +23,28 @@ document.getElementById("form").addEventListener("click", function(event) {
 //calls showForm function when user clicks in clickable-area container
 document.getElementById("clickable-area").addEventListener("click", showForm);
 
+
+// function that adds the user input from the form to an object then pushes 
+// that opject to an array of form answers
+
+let formArray = []
+
+function setContent() {
+    let nameValue = document.getElementById("name").value;
+    let dateOfBirth = document.getElementById("age").value;
+    let messageValue = document.getElementById("message").value;
+
+    let formObj = {name: nameValue, dob: dateOfBirth, message: messageValue}
+
+    formArray.push(formObj);
+    console.log(formArray);
+
+}
+
+
+//calls setContent function when the submit button is clicked
+document.getElementById("btn").addEventListener("click", setContent);
+
 //calls hideForm function when the submit button is clicked
 document.getElementById("btn").addEventListener("click", hideForm);
 
@@ -36,6 +53,15 @@ document.getElementById("btn-close").addEventListener("click", hideForm);
 
 
 
-
-
-
+//TO DO:
+//1. form validation
+//2. figure out how to add the location of the click that brings up
+//the form to the object so I can later display that message at the
+//spot the user clicked
+//3. Display each message where the user clicked
+//4. Calculate the users life cycle stage from DOB and display
+//image of the life cycle stage at the top corner of the message
+//the life cycle stage will need to be calculated each time the pg loads
+//and not set as a value in the object becuase the current cycle will change
+//5. make keyboard that represents peduncle borer communication better
+//than human letters
